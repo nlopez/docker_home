ed3621b21cfc1ac1e6a67248e8896ae7724ad838
Nick Lopez <nlopez@gmail.com>
2017-02-05 17:16:01 +0000

    add terraform bits

---
+++ terraform/docker.tf
@@ -0,0 +1,18 @@
+module "cecil" {
+  source = "./module/docker_host"
+  name = "cecil"
+  ip_range = "172.16.9.0/24"
+  parent = "eth1"
+}
+
+module "biggs" {
+  source = "./module/docker_host"
+  name = "cecil"
+  ip_range = "172.16.10.0/24"
+}
+
+module "wedge" {
+  source = "./module/docker_host"
+  name = "wedge"
+  ip_range = "172.16.11.0/24"
+}
---
+++ terraform/module/docker_host/docker_host.tf
@@ -0,0 +1,27 @@
+variable "name" {}
+variable "domain" { default = "radoncanyon.com" }
+variable "port" { default = 2376 }
+variable "scheme" { default = "tcp" }
+variable "subnet" { default = "172.16.8.0/22" }
+variable "ip_range" {}
+variable "parent" { default = "eno1" }
+variable "gateway" { default = "172.16.8.1" }
+
+provider "docker" {
+  alias = "${var.name}"
+  host = "${var.scheme}://${var.name}.${var.domain}:${var.port}"
+}
+
+resource "docker_network" "physical" {
+  provider = "docker.${var.name}"
+  name = "physical"
+  driver = "macvlan"
+  options = {
+    parent = "${var.parent}"
+  }
+  ipam_config = {
+    ip_range = "${var.ip_range}"
+    subnet = "${var.subnet}"
+    gateway = "${var.gateway}"
+  }
+}
