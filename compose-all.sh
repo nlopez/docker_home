84d24d32d94f5b17ad4ee84a07ba5d4eced24af0
Nick Lopez <nlopez@gmail.com>
2017-02-08 09:09:50 +0000

    rely on defaults

--- compose-all.sh
+++ compose-all.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 hosts=(cecil biggs wedge)
 (
   for host in "${hosts[@]}"; do
-    command="source "$host/.envrc"; docker-compose"
+    command="cd $here/$host; source ./.envrc; docker-compose"
     while read -r -d $'\0'; do
       command+=" -f $REPLY"
-    done < <(find -E "$host" -type f -regex '.*\.ya?ml$' -print0)
+    done < <(find -E "$here/$host" -type f -regex '.*\.ya?ml$' -print0)
     echo "$command $*"
   done
 ) | xargs -P${#hosts[@]} -I{} sh -c '{}'
