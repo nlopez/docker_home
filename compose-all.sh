3d242fdb154a45fa4291a8559ce09f77184b1f3b
Nick Lopez <nlopez@gmail.com>
2017-02-08 06:13:03 +0000

    make compose-all more generic, remove stray compose file

--- compose-all.sh
+++ compose-all.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 hosts=(cecil biggs wedge)
 (
-  for host in ${hosts[@]}; do
+  for host in "${hosts[@]}"; do
     command="source "$host/.envrc"; docker-compose"
     while read -r -d $'\0'; do
       command+=" -f $REPLY"
-    done < <(find -E "$host" -type f -regex '.*/[0-9]{2}-.*\.yaml$' -print0)
-    echo "$command $@"
+    done < <(find -E "$host" -type f -regex '.*\.ya?ml$' -print0)
+    echo "$command $*"
   done
 ) | xargs -P${#hosts[@]} -I{} sh -c '{}'
